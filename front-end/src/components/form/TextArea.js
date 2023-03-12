8f86aa20a97e0ae0eea1e01a5339b7f6721da06f
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2023-03-12 17:51:41 -0400

    add more form component

---
+++ front-end/src/components/form/TextArea.js
@@ -0,0 +1,20 @@
+const TextArea = (props) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={props.name} className="form-label">
+        {props.title}
+      </label>
+      <textarea 
+        className="form-control"
+        name={props.name} 
+        id={props.name} 
+        value={props.value} 
+        onChange={props.onChange}
+        rows={props.rows}
+      />
+      <div className={props.errorDiv}>{props.errorMessage}</div>
+    </div>
+  )
+}
+
+export default TextArea;
